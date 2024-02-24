367bc89c924b3f0f2a350fe93320030fb070838e
@kavinduushan07 <kavinduushan07@gmail.com>
2024-02-25 01:05:08 +0530

    complete create listing page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -8,6 +8,7 @@ import {
 } from "firebase/storage";
 import { app } from "../firebase";
 import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserStart } from "../redux/user/userSlice";
+import {Link}  from 'react-router-dom'
 
 export default function Profile() {
   const fileRef = useRef(null);
@@ -182,6 +183,10 @@ export default function Profile() {
         >
           {loading ? 'Loading...' : 'Update'}
         </button>
+
+        <Link className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95" to={'/create-listing'}>
+        Create Listing
+        </Link>
       </form>
 
       <div className="flex justify-between mt-5">
